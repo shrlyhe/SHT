16f30c98c769f643f750c2f147c1f24005d98c8b
shrlyhe <she@wesleyan.edu>
2017-02-12 10:43:48 -0500

    fix heroku config

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -16,7 +16,8 @@ var firebase = require('firebase');
 
 var client_id = '6020c8fabb1c4ee49643850c17e4d18f'; // Your client id
 var client_secret = 'ee07ab41c0d146f084468e4c4f378afc'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var hostname = process.env.HOSTNAME || "localhost:8888"
+var redirect_uri = hostname + '/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
