2645555246721282f3b51f4d7dc90b90c4fd37b7
shrlyhe <she@wesleyan.edu>
2017-02-12 09:24:37 -0500

    make port configurable

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -143,5 +143,6 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
-console.log('Listening on 8888');
-app.listen(8888);
+var port = process.env.PORT || 8888;
+console.log('Listening on ' + port);
+app.listen(port);
